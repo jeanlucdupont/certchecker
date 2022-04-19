6059c48f1e58f374437feef57e3dc122867c6579
Jean-Luc Dupont <103344686+jeanlucdupont@users.noreply.github.com>
2022-04-18 22:27:04 -0400

    Add files via upload

--- www/index.php
+++ www/index.php
@@ -1,6 +1,19 @@
 <?php
 require 'option.php';
 
+$request  = "select * from `site`";
+$ans      = mysqli_query($connect, $request);
+$smarty->assign('nbsite', mysqli_num_rows($ans));
+
+$request  = "select * from `site`";
+$ans      = mysqli_query($connect, $request);
+$biglist = array();
+while ($fields = mysqli_fetch_array($ans))
+{
+  array_push($biglist, $fields);
+}
+$smarty->assign('biglist', $biglist);
+
 $smarty->display('index.tpl');
 mysqli_close($connect);
 
