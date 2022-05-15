8e498ea1d9321216ca280e12a24fbaa5fb1e7fa5
Jean-Luc Dupont <103344686+jeanlucdupont@users.noreply.github.com>
2022-05-15 13:50:10 -0400

    Add files via upload

--- www/settings.php
+++ www/settings.php
@@ -1,6 +1,23 @@
 <?php
 require 'option.php';
 
+
+foreach ($_POST as $key => $value) 
+{
+	$porc = substr(htmlspecialchars($key), 0, 1);
+	$porcid = substr(htmlspecialchars($key), 1);
+	if ($porc == 'p')
+	{
+		mysqli_query($connect, "update sslsuite set ok = " . $value . " where sslsuite_id = " . $porcid);
+	}
+	if ($porc == 'c')
+	{
+		mysqli_query($connect, "update cyphersuite set ok = " . $value . " where cyphersuite_id = " . $porcid);
+		
+	}		   
+}
+
+
 $request  = "select * from `sslsuite` order by ok desc, `sslsuite_id` desc;";
 $ans      = mysqli_query($connect, $request);
 $protlist = array();
@@ -17,6 +34,8 @@ while ($fields = mysqli_fetch_array($ans))
 }
 $smarty->assign('cypherlist', $cypherlist);
 $smarty->assign('protlist', $protlist);
+
+
 
 $smarty->display('settings.tpl');
 mysqli_close($connect);
